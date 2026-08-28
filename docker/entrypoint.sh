ee474db5329e14ccaa437670a9f4e9026a343921
DevooKim <khwoo8026@gmail.com>
2026-08-28 13:09:05 +0900

    fix: enforce compose secret fail-fast

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,11 +17,18 @@ secret_value() {
   printf '%s' "$value"
 }
 
-export AWS_ACCESS_KEY_ID="$(secret_value aws_access_key_id)"
-export AWS_SECRET_ACCESS_KEY="$(secret_value aws_secret_access_key)"
-export SPOTIFY_CLIENT_ID="$(secret_value spotify_client_id)"
-export SPOTIFY_CLIENT_SECRET="$(secret_value spotify_client_secret)"
-export SPOTIFY_REFRESH_TOKEN="$(secret_value spotify_refresh_token)"
-export REVALIDATE_SECRET="$(secret_value revalidate_secret)"
+AWS_ACCESS_KEY_ID_VALUE="$(secret_value aws_access_key_id)" || exit 1
+AWS_SECRET_ACCESS_KEY_VALUE="$(secret_value aws_secret_access_key)" || exit 1
+SPOTIFY_CLIENT_ID_VALUE="$(secret_value spotify_client_id)" || exit 1
+SPOTIFY_CLIENT_SECRET_VALUE="$(secret_value spotify_client_secret)" || exit 1
+SPOTIFY_REFRESH_TOKEN_VALUE="$(secret_value spotify_refresh_token)" || exit 1
+REVALIDATE_SECRET_VALUE="$(secret_value revalidate_secret)" || exit 1
+
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_VALUE"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY_VALUE"
+export SPOTIFY_CLIENT_ID="$SPOTIFY_CLIENT_ID_VALUE"
+export SPOTIFY_CLIENT_SECRET="$SPOTIFY_CLIENT_SECRET_VALUE"
+export SPOTIFY_REFRESH_TOKEN="$SPOTIFY_REFRESH_TOKEN_VALUE"
+export REVALIDATE_SECRET="$REVALIDATE_SECRET_VALUE"
 
 exec "$@"
